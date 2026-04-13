0139665e0484213299908166d4d2320b7cd172cb
deanyao6 <dyao2@caltech.edu>
2026-04-13 09:40:06 -0700

    test

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
 import os
 import pandas as pd
 
-# cum round
-
 train_csv = "/resnick/groups/CS156b/from_central/data/student_labels/train2023.csv"
 
 df = pd.read_csv(train_csv)
 
 print(df.columns.tolist())
 print("num rows:", len(df))
+print(df.head())
