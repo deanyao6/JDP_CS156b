3c7b82719dfbfadcfa5c850f99549ee7a76c3c67
deanyao6 <dyao2@caltech.edu>
2026-04-13 02:03:47 -0700

    data file

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-# hello
+import os
+import pandas as pd
+
+train_csv = "/resnick/groups/CS156b/from_central/data/student_labels/train2023.csv"
+
+df = pd.read_csv(train_csv)
+
+print(df.columns.tolist())
+print("num rows:", len(df))
