d3020585db7b699799aaa3714c140815318ff622
jennawang <jwang8@caltech.edu>
2026-04-15 12:32:19 -0700

    fixing training .py

--- resnet_frontal_training.py
+++ resnet_frontal_training.py
@@ -48,7 +48,7 @@ def get_transforms(split="train", img_size=224):
     if split == "train":
         return transforms.Compose([
             # transforms.Resize(256),
-            transforms.Resize(224),
+            transforms.Resize((img_size, img_size)),
             # transforms.CenterCrop(img_size),
             transforms.RandomHorizontalFlip(),
             transforms.RandomRotation(10),
@@ -58,7 +58,7 @@ def get_transforms(split="train", img_size=224):
         ])
     else:
         return transforms.Compose([
-            transforms.Resize(224),
+            transforms.Resize((img_size, img_size)),
             # transforms.CenterCrop(img_size),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
