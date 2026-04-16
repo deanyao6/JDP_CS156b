c5c48b93b18a93dce9d5b1e5a1a38746e9844465
piotrzawislan <pzawisla@caltech.edu>
2026-04-15 17:32:04 -0700

    test

---
+++ model1.py
@@ -0,0 +1,9 @@
+import os
+import pandas as pd
+import numpy as np
+from PIL import Image, ImageOps
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+print("Hi")
