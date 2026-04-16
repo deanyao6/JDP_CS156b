31d979abeb143463f5e73acefb6195242e3ae5be
Dean Yao <dyao2@caltech.edu>
2026-04-15 20:22:58 -0700

    changed densenet to be first 5000 picutres - subset

--- test.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd, os
-df = pd.read_csv('frontal_train.csv')
-sample = df.iloc[0]['Path'].replace('CheXpert-v1.0/', '')
-full = os.path.join('/resnick/groups/CS156b/from_central/data', sample)
-print(os.path.exists(full))  # should print True
