3614f5f4dbdf33f34d229736ff52227e32cbacd2
jennawang <jwang8@caltech.edu>
2026-04-17 18:13:16 -0700

    sbatch

--- data_cleaning.sh
+++ data_cleaning.sh
@@ -4,9 +4,12 @@
 #SBATCH --mem=8G
 #SBATCH --time=00:30:00
 #SBATCH -J "data_cleaning"
+#SBATCH --mail-user=jwang8@caltech.edu 
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
 
 cd /resnick/groups/CS156b/from_central/2026/JDP/jenna
-git pull origin main
 
 source ~/miniconda3/bin/activate cs156b
 python data_cleaning.py
