3e5e99cc8131b07556ab2e116b15c52a184c53b8
jennawang <jwang8@caltech.edu>
2026-04-25 18:20:26 -0700

    auc fix

--- resnet_frontal_training.py
+++ resnet_frontal_training.py
@@ -122,7 +122,7 @@ def evaluate(model, loader, criterion, device):
         valid = ~np.isnan(all_labels[:, i])
         if valid.sum() > 0 and len(np.unique(all_labels[valid, i])) > 1:
             binary_labels = (all_labels[valid, i] == 1).astype(float)
-            aucs[col] = roc_auc_score(all_labels[valid, i], all_preds[valid, i])
+            aucs[col] = roc_auc_score(binary_labels, all_preds[valid, i])
         else:
             aucs[col] = float("nan")
 
