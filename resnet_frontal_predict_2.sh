9c0936636add6b1067cf6d6651a85a0e0f3ae779
jennawang <jwang8@caltech.edu>
2026-05-03 13:49:41 -0700

    predict

--- resnet_frontal_predict_2.sh
+++ resnet_frontal_predict_2.sh
@@ -6,9 +6,9 @@
 #SBATCH --gres=gpu:h100:1
 #SBATCH --mem=16G
 #SBATCH --time=00:30:00
-#SBATCH -J "predict"
-#SBATCH --output=predict_%j.out
-#SBATCH --error=predict_%j.err
+#SBATCH -J "frontal_resnet_2"
+#SBATCH --output=resnet_frontal_predict_2_%j.out
+#SBATCH --error=resnet_frontal_predict_2_%j.err
 #SBATCH --mail-user=jwang8@caltech.edu 
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
@@ -18,4 +18,4 @@ cd /resnick/groups/CS156b/from_central/2026/JDP/jenna
 source ~/miniconda3/bin/activate cs156b
 python resnet_frontal_predict.py \
     --checkpoint checkpoints/resnet_frontal_training_2_h200/best_resnet50.pth \
-    --output submission_resnet_frontal_training_2_h200.csv
+    --output submission_resnet_frontal_2_h200.csv
